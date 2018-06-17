c7cb7403bbf28eacdbc1def2929e99c6d1a689b8
Jesse McReady <jessemcready@gmail.com>
2018-06-17 19:00:19 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,3 +2,30 @@ def instantiate_new_array
   
 end
 
+def array_with_two_elements
+  
+end
+
+def first_element(arr)
+  
+end
+
+def third_element(arr)
+  
+end
+
+def last_element(arr)
+  
+end
+
+def first_element_with_array_methods(arr)
+  
+end
+
+def last_element_with_array_methods(arr)
+  
+end
+
+def length_of_array(arr)
+  
+end
